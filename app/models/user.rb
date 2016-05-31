e13201c1801750d6b39ad9e15ba6839faad84852
sashastip <saschastipanenko@gmail.com>
2016-05-31 12:43:27 +0300

    added login bouth email or username

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,27 @@ class User < ApplicationRecord
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
 
   validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
+  validates_uniqueness_of :username
+  validates_presence_of :username
+  validates :username, length: { in: 4..20 }
+
+  attr_accessor :login
+
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
 end
