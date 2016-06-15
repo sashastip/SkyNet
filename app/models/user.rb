2b9d3ee46a7449896f437b9becafb6adc8a4c68e
sashastip <saschastipanenko@gmail.com>
2016-06-15 15:16:28 +0300

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   rolify
   has_and_belongs_to_many :friends
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
